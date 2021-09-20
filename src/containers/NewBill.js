3bb5b51a48139745d1a969d544086bca1040ac76
Amar <a.zirouel@gmail.com>
2021-09-20 10:49:39 +0200

    résolution du [Bug Hunt] - Bills

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -11,6 +11,7 @@ export default class NewBill {
     formNewBill.addEventListener("submit", this.handleSubmit)
     const file = this.document.querySelector(`input[data-testid="file"]`)
     file.addEventListener("change", this.handleChangeFile)
+
     this.fileUrl = null
     this.fileName = null
     new Logout({ document, localStorage, onNavigate })
@@ -19,7 +20,9 @@ export default class NewBill {
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
-    this.firestore
+    const fileExtension = fileName.split(".")[fileName.split(".").length-1]
+    if(fileExtension ==="jpg" || fileExtension ==="jpeg" || fileExtension ==="png"){
+      this.firestore
       .storage
       .ref(`justificatifs/${fileName}`)
       .put(file)
@@ -28,6 +31,12 @@ export default class NewBill {
         this.fileUrl = url
         this.fileName = fileName
       })
+      this.document.querySelector(".error-msg").classList.remove("visible")
+    } else{
+      this.document.querySelector(".error-msg").classList.add("visible")
+      this.document.querySelector(`input[data-testid="file"]`).value = null;
+    }
+
   }
   handleSubmit = e => {
     e.preventDefault()
